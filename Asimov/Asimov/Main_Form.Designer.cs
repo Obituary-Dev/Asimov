fc360de856d5b5aedc9bef602366e2a9c371afde
TheoLaka <66211417+TheoLaka@users.noreply.github.com>
2021-04-19 15:24:24 +0200

    Ajout Menu dans l'accueil

--- Asimov/Asimov/Main_Form.Designer.cs
+++ Asimov/Asimov/Main_Form.Designer.cs
@@ -29,41 +29,95 @@ namespace Asimov
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.profilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.professeurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matièreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.classesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.élèveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // contextMenuStrip1
+            // menuStrip1
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.profilToolStripMenuItem,
+            this.professeurToolStripMenuItem,
+            this.matièreToolStripMenuItem,
+            this.notesToolStripMenuItem,
+            this.classesToolStripMenuItem,
+            this.élèveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 2, 0, 2);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(764, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // openToolStripMenuItem
+            // profilToolStripMenuItem
             // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
-            this.openToolStripMenuItem.Text = "Open";
+            this.profilToolStripMenuItem.Name = "profilToolStripMenuItem";
+            this.profilToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.profilToolStripMenuItem.Text = "Profil";
+            // 
+            // professeurToolStripMenuItem
+            // 
+            this.professeurToolStripMenuItem.Name = "professeurToolStripMenuItem";
+            this.professeurToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.professeurToolStripMenuItem.Text = "Professeur";
+            // 
+            // matièreToolStripMenuItem
+            // 
+            this.matièreToolStripMenuItem.Name = "matièreToolStripMenuItem";
+            this.matièreToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.matièreToolStripMenuItem.Text = "Matière";
+            // 
+            // notesToolStripMenuItem
+            // 
+            this.notesToolStripMenuItem.Name = "notesToolStripMenuItem";
+            this.notesToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.notesToolStripMenuItem.Text = "Notes";
+            // 
+            // classesToolStripMenuItem
+            // 
+            this.classesToolStripMenuItem.Name = "classesToolStripMenuItem";
+            this.classesToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.classesToolStripMenuItem.Text = "Classes";
+            // 
+            // élèveToolStripMenuItem
+            // 
+            this.élèveToolStripMenuItem.Name = "élèveToolStripMenuItem";
+            this.élèveToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.élèveToolStripMenuItem.Text = "Élève";
             // 
             // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(764, 491);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main_Form";
-            this.Text = "Form1";
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.Text = "Main Page";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem profilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem professeurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matièreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem notesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem classesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem élèveToolStripMenuItem;
     }
 }
 
